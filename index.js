fac682607f4199714bb1a5400a4f9f1950983267
Shamim Reza <wpshamim@gmail.com>
2022-06-27 05:06:53 +0600

    implement jwt token and verify login user

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const jwtVerify = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Error 401 - Unauthorized!" });
+    }
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_ACCESS_TOKEN, async (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Error 403 - Forbidden" });
+        } else {
+            req.decoded = decoded;
+        }
+        await next();
+    });
+};
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ssehx4e.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
